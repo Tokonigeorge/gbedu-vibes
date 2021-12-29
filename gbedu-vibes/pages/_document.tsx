e6e482e668911639d41ebbcc83a402b5aa074fcf
Bukola <bukolaebikefe@yahoo.com>
2021-12-29 14:17:19 +0100

    set up font hind and gilgongo

--- gbedu-vibes/pages/_document.tsx
+++ gbedu-vibes/pages/_document.tsx
@@ -7,8 +7,19 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head>
           <link
+            as="font"
+            href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Hind:wght@300;400;500;600;700&family=Montserrat:wght@200&display=swap"
             rel="preload"
-            href="/fonts/Graphik/Graphik-Light.ttf"
+          />
+          <link
+            rel="preload"
+            href="/fonts/BackOutWeb.otf"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/OutOfAfrica.ttf"
             as="font"
             crossOrigin=""
           />
