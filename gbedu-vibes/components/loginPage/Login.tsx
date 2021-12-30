bfb9eeb09da746dc38f2f7f730281e311ed57bf6
Bukola <bukolaebikefe@yahoo.com>
2021-12-30 15:18:26 +0100

    added spotify auth

--- gbedu-vibes/components/loginPage/Login.tsx
+++ gbedu-vibes/components/loginPage/Login.tsx
@@ -1,10 +1,12 @@
+import { loginUrl } from "../../utils/spotify-auth";
+
 export default function Login() {
   return (
-    <button
-      type="button"
+    <a
+      href={loginUrl}
       className="font-semibold text-white ease-in hover:text-spotifygreen bg-spotifygreen text-xs py-3 hover:bg-white hover:drop-shadow-xl px-16 rounded-full text-center"
     >
       CONNECT WITH SPOTIFY
-    </button>
+    </a>
   );
 }
---
+++ gbedu-vibes/utils/spotify-auth.tsx
@@ -0,0 +1,26 @@
+export const endpoint: string = "https://accounts.spotify.com/authorize";
+const clientId: string = "47330e5a65544359b3c736707cf071ed"; //set to an env variable
+const redirectUri: string = "http://localhost:3000/";
+const scopes: string[] = [
+  "user-library-read",
+  "user-library-modify",
+  "playlist-modify-public",
+  "playlist-read-private",
+  "playlist-read-collaborative",
+  "playlist-modify-private",
+];
+
+export const getToken = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((i, item) => {
+      let parts = item.split("=");
+      i[parts[0]] = decodeURIComponent(parts[1]);
+      return i;
+    }, {});
+};
+
+export const loginUrl = `${endpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
