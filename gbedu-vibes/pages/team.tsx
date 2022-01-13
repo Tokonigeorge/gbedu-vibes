0d5bc21b8d7b0479fbfb28b26630570281e8c5cc
Bukola <bukolaebikefe@yahoo.com>
2022-01-14 00:21:59 +0100

    worked on layout blob page

--- gbedu-vibes/pages/team.tsx
+++ gbedu-vibes/pages/team.tsx
@@ -2,21 +2,24 @@ import Nav from "../components/Nav";
 import TeamMember from "../components/teamPage/TeamMember";
 import avatarfemale from "../public/avatarfemale.svg";
 import avatarmale from "../public/avatarmale.svg";
+import Layout from "../components/Layout";
 
 export default function Team() {
   return (
     <div className="pt-6 px-6 md:px-24 sm:px-12 bg-background h-screen">
-      <Nav showTeam={true} />
-      <TeamMember
-        avatar={avatarfemale}
-        name="Ebikefe, Bukola Tokoni"
-        position="Software Developer"
-      />
-      <TeamMember
-        avatar={avatarmale}
-        name="Nlewedim, Chisombiri Leonard"
-        position="Product Designer"
-      />
+      <Layout>
+        <Nav showTeam={true} />
+        <TeamMember
+          avatar={avatarfemale}
+          name="Ebikefe, Bukola Tokoni"
+          position="Software Developer"
+        />
+        <TeamMember
+          avatar={avatarmale}
+          name="Nlewedim, Chisombiri Leonard"
+          position="Product Designer"
+        />
+      </Layout>
     </div>
   );
 }
