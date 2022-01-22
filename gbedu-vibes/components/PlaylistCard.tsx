3150e58fc6d35db15401d9a7f3e8928cc83cd432
Bukola <bukolaebikefe@yahoo.com>
2022-01-22 01:52:26 +0100

    setup user playlists

--- gbedu-vibes/components/PlaylistCard.tsx
+++ gbedu-vibes/components/PlaylistCard.tsx
@@ -9,14 +9,16 @@ export default function PlaylistCard({
   des: string;
 }) {
   return (
-    <div className="w-52 bg-red-200 rounded-md flex flex-col items-center justify-center py-3">
+    <div className=" bg-offWhite rounded-md flex flex-col items-center py-3">
       <div className="rounded-md">
         <Image src="/playlistimage.svg" width={200} height={200} />
       </div>
-      <div className="-mt-3">
-        {name && <p className="font-semibold">{name}</p>}
+      <div className="-mt-2 pl-5 self-start">
+        {name && (
+          <p className="font-semibold text-sm  text-left mb-1">{name}</p>
+        )}
         {des && (
-          <p className="text-sm text-black text-opacity-60">
+          <p className="text-xs text-black text-opacity-60 text-left">
             {cutString(des, 23)}
           </p>
         )}
--- gbedu-vibes/pages/mood.tsx
+++ gbedu-vibes/pages/mood.tsx
@@ -12,7 +12,7 @@ export default function Mood() {
   const [data, setData] = useState(null);
   const [{ user }] = useDataContextVal();
   /*check if onreload user gets clear, save user data in the session storage? */
-
+  console.log(data);
   useEffect(() => {
     setLoading(true);
     spotifyWeb
@@ -28,9 +28,9 @@ export default function Mood() {
     <Layout>
       <div className="bg-background pt-6 px-6 md:px-20 sm:px-12">
         <Nav showTeam={false} />
-        <div className="mt-16">
+        <div className="mt-8">
           <p className="font-semibold text-xl">
-            Hi{(user?.display_name && ",") || ","}
+            {user?.display_name ? `${user?.display_name},` : "Hi,"}
           </p>
           <p className="text-sm text-black text-opacity-60 pt-4">
             Click a playlist to analyze.
@@ -38,8 +38,14 @@ export default function Mood() {
         </div>
         {/* {loading && <p>Loading...</p>}
         {!data && !loading && <p>No data</p>} */}
-        <div className="pt-10">
-          <PlaylistCard name="name" des="A spotify something something" />
+        <div className="py-10 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-x-10 gap-y-8">
+          {data?.map((i, indx) => (
+            <PlaylistCard
+              name={i.name}
+              des={i.owner?.display_name}
+              key={indx}
+            />
+          ))}
         </div>
       </div>
     </Layout>
